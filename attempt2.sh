29eef8aa8a769f477a613b59e7526f92da3488e0
John Cosnett <johncosnett@Johns-iMac.local>
2017-07-06 19:08:55 +0100

    asdf

---
+++ attempt2.sh
@@ -0,0 +1,44 @@
+# location of where to place the flowers data
+FLOWERS_DATA_DIR=/tmp/flowers-data/
+
+# build the preprocessing script.
+cd tensorflow-models/inception
+bazel build //inception:download_and_preprocess_flowers
+
+# run it
+bazel-bin/inception/download_and_preprocess_flowers "${FLOWERS_DATA_DIR}"
+
+
+# location of where to place the Inception v3 model
+INCEPTION_MODEL_DIR=$HOME/inception-v3-model
+mkdir -p ${INCEPTION_MODEL_DIR}
+cd ${INCEPTION_MODEL_DIR}
+
+# download the Inception v3 model
+curl -O http://download.tensorflow.org/models/image/imagenet/inception-v3-2016-03-01.tar.gz
+tar xzf inception-v3-2016-03-01.tar.gz
+
+
+# Build the model. Note that we need to make sure the TensorFlow is ready to
+# use before this as this command will not build TensorFlow.
+cd tensorflow-models/inception
+bazel build //inception:flowers_train
+
+# Path to the downloaded Inception-v3 model.
+MODEL_PATH="${INCEPTION_MODEL_DIR}/inception-v3/model.ckpt-157585"
+
+# Directory where the flowers data resides.
+FLOWERS_DATA_DIR=/tmp/flowers-data/
+
+# Directory where to save the checkpoint and events files.
+TRAIN_DIR=/tmp/flowers_train/
+
+# Run the fine-tuning on the flowers data set starting from the pre-trained
+# Imagenet-v3 model.
+bazel-bin/inception/flowers_train \
+  --train_dir="${TRAIN_DIR}" \
+  --data_dir="${FLOWERS_DATA_DIR}" \
+  --pretrained_model_checkpoint_path="${MODEL_PATH}" \
+  --fine_tune=True \
+  --initial_learning_rate=0.001 \
+  --input_queue_memory_factor=1
