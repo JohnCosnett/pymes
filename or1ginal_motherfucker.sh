d601061a12dac1246513598d8c1cfb0ffbac7746
John Cosnett <johncosnett@Johns-iMac.local>
2017-07-06 17:26:43 +0100

    asdf

--- or1ginal_motherfucker.sh
+++ or1ginal_motherfucker.sh
@@ -20,40 +20,40 @@ cd ~/cube_0/models/inception
 #pause '___________--2'
 
 bazel build //inception:download_and_preprocess_flowers
-#pause '___________--3'
+pause '___________--3'
 # run it
 bazel-bin/inception/download_and_preprocess_flowers "${FLOWERS_DATA_DIR2}"
-#pause '___________--4'
+pause '___________--4'
 
 
 
 # location of where to place the Inception v3 model
 INCEPTION_MODEL_DIR=$HOME/inception-v3-model
-#pause '___________--5'
+pause '___________--5'
 
 
 mkdir -p ${INCEPTION_MODEL_DIR}
-#pause '___________--6'
+pause '___________--6'
 
 cd ${INCEPTION_MODEL_DIR}
-#pause '___________--7'
+pause '___________--7'
 
 # download the Inception v3 model
 curl -O http://download.tensorflow.org/models/image/imagenet/inception-v3-2016-03-01.tar.gz
-#pause '___________--8'
+pause '___________--8'
 
 tar xzf inception-v3-2016-03-01.tar.gz
-#pause '___________--9'
+pause '___________--9'
 
 # this will create a directory called inception-v3 which contains the following files.
 > ls inception-v3
-#pause '___________--10'
+pause '___________--10'
 README.txt
-#pause '___________--11'
+pause '___________--11'
 checkpoint
-#pause '___________--12'
+pause '___________--12'
 model.ckpt-157585
-#pause '___________--13'
+pause '___________--13'
 
 
 
@@ -61,23 +61,23 @@ model.ckpt-157585
 # Build the model. Note that we need to make sure the TensorFlow is ready to
 # use before this as this command will not build TensorFlow.
 cd ~/cube_0/models/inception
-#pause '___________--14'
+pause '___________--14'
 bazel build //inception:flowers_train
-#pause '___________--15'
+pause '___________--15'
 
 # Path to the downloaded Inception-v3 model.
 MODEL_PATH="${INCEPTION_MODEL_DIR}/inception-v3/model.ckpt-157585"
-#pause '___________--16'
+pause '___________--16'
 
 # Directory where the flowers data resides.
 FLOWERS_DATA_DIR=/tmp/flowers-data/
 FLOWERS_DATA_DIR2=$HOME/flowers-data/
-#pause '___________--17'
+pause '___________--17'
 
 # Directory where to save the checkpoint and events files.
 TRAIN_DIR=/tmp/flowers_train/
 TRAIN_DIR2=$HOME/flowers_train/
-#pause '___________--18'
+pause '___________--18'
 
 # Run the fine-tuning on the flowers data set starting from the pre-trained
 # Imagenet-v3 model.
@@ -89,4 +89,4 @@ bazel-bin/inception/flowers_train \
   --initial_learning_rate=0.001 \
   --input_queue_memory_factor=1
 
-#pause '___________--19'
+pause '___________--19'
